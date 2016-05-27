2b8173cb438fdac4dadc0bf7c72aebaea566c98c
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-27 12:57:05 -0700

    1257

--- views/project_main.controller.js
+++ views/project_main.controller.js
@@ -18,6 +18,41 @@ sap.ui.controller("project.views.project_main", {
                 {"Year": "2014","Value": "1274018495"},
                ]};
     oModel.setData(data);
+
+    var oDataset = new sap.viz.ui5.data.FlattenedDataset({
+      dimensions : [{
+        name : 'Year',
+        value : "{Year}"}],
+                     
+      measures : [{
+        name : 'Population',
+        value : '{Value}'} ],
+                   
+      data : {
+        path : "/Population"
+      }
+    });   
+    oVizFrame.setDataset(oDataset);
+    oVizFrame.setModel(oModel); 
+    oVizFrame.setVizType('column');
+
+    oVizFrame.setVizProperties({
+            plotArea: {
+              colorPalette : d3.scale.category20().range()
+                }});
+    
+    var feedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+          'uid': "valueAxis",
+          'type': "Measure",
+          'values': ["Population"]
+        }), 
+        feedCategoryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+          'uid': "categoryAxis",
+          'type': "Dimension",
+          'values': ["Year"]
+        });
+    oVizFrame.addFeed(feedValueAxis);
+    oVizFrame.addFeed(feedCategoryAxis);
   },
 
   // _handleRouteMatched:function(evt){
