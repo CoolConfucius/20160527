3258f0f9a11b115106b6a7521ee7d503ce15909d
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-27 12:58:55 -0700

    chart is showing now refactor to a function

--- views/project_main.controller.js
+++ views/project_main.controller.js
@@ -7,52 +7,7 @@ sap.ui.controller("project.views.project_main", {
     // this.list.attachItemPress(this.listSelection, this);
     this.byId("list").attachItemPress(this.listSelection, this);
     this.fetchdata(); 
-    var oVizFrame = this.getView().byId("idcolumn");
-    var oModel = new sap.ui.model.json.JSONModel();
-    var data = {
-        'Population' : [
-                {"Year": "2010","Value": "158626687"},
-                {"Year": "2011","Value": "531160986"},
-                {"Year": "2012","Value": "915105168"},
-                {"Year": "2013","Value": "1093786762"},
-                {"Year": "2014","Value": "1274018495"},
-               ]};
-    oModel.setData(data);
-
-    var oDataset = new sap.viz.ui5.data.FlattenedDataset({
-      dimensions : [{
-        name : 'Year',
-        value : "{Year}"}],
-                     
-      measures : [{
-        name : 'Population',
-        value : '{Value}'} ],
-                   
-      data : {
-        path : "/Population"
-      }
-    });   
-    oVizFrame.setDataset(oDataset);
-    oVizFrame.setModel(oModel); 
-    oVizFrame.setVizType('column');
-
-    oVizFrame.setVizProperties({
-            plotArea: {
-              colorPalette : d3.scale.category20().range()
-                }});
-    
-    var feedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
-          'uid': "valueAxis",
-          'type': "Measure",
-          'values': ["Population"]
-        }), 
-        feedCategoryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
-          'uid': "categoryAxis",
-          'type': "Dimension",
-          'values': ["Year"]
-        });
-    oVizFrame.addFeed(feedValueAxis);
-    oVizFrame.addFeed(feedCategoryAxis);
+    this.makeChart();
   },
 
   // _handleRouteMatched:function(evt){
@@ -135,5 +90,54 @@ sap.ui.controller("project.views.project_main", {
     this.otableModel.setData(this.tablearray);
     // this.table.setModel(this.otableModel);
     this.byId("table").setModel(this.otableModel);
+  }, 
+
+  makeChart: function() {
+    var oVizFrame = this.getView().byId("idcolumn");
+    var oModel = new sap.ui.model.json.JSONModel();
+    var data = {
+        'Population' : [
+                {"Year": "2010","Value": "158626687"},
+                {"Year": "2011","Value": "531160986"},
+                {"Year": "2012","Value": "915105168"},
+                {"Year": "2013","Value": "1093786762"},
+                {"Year": "2014","Value": "1274018495"},
+               ]};
+    oModel.setData(data);
+
+    var oDataset = new sap.viz.ui5.data.FlattenedDataset({
+      dimensions : [{
+        name : 'Year',
+        value : "{Year}"}],
+                     
+      measures : [{
+        name : 'Population',
+        value : '{Value}'} ],
+                   
+      data : {
+        path : "/Population"
+      }
+    });   
+    oVizFrame.setDataset(oDataset);
+    oVizFrame.setModel(oModel); 
+    oVizFrame.setVizType('column');
+
+    oVizFrame.setVizProperties({
+            plotArea: {
+              colorPalette : d3.scale.category20().range()
+                }});
+    
+    var feedValueAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+          'uid': "valueAxis",
+          'type': "Measure",
+          'values': ["Population"]
+        }), 
+        feedCategoryAxis = new sap.viz.ui5.controls.common.feeds.FeedItem({
+          'uid': "categoryAxis",
+          'type': "Dimension",
+          'values': ["Year"]
+        });
+    oVizFrame.addFeed(feedValueAxis);
+    oVizFrame.addFeed(feedCategoryAxis);  
   }
 })
